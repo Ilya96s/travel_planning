3b7ad0ef1a4cefed75bcbb2502d6c271dc6879f1
ilya96s <i.kaltygin@icloud.com>
2025-02-02 13:08:46 +0700

    2.2 configured the circuit breaker and retry

--- travel-plans-service/src/main/java/ru/jg/travelplans/client/AttractionOpenFeignClient.java
+++ travel-plans-service/src/main/java/ru/jg/travelplans/client/AttractionOpenFeignClient.java
@@ -1,5 +1,7 @@
 package ru.jg.travelplans.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,11 @@ import ru.jg.travelplans.dto.AttractionResponseDto;
 public interface AttractionOpenFeignClient {
 
     @GetMapping("/api/v1/attractions/{attractionId}")
+    @Retry(name = "attractionCB", fallbackMethod = "getAttractionByIdFallbackMethod")
+    @CircuitBreaker(name = "attractionCB", fallbackMethod = "getAttractionByIdFallbackMethod")
     AttractionResponseDto getAttractionById(@PathVariable("attractionId") Long attractionId);
 
+    default AttractionResponseDto getAttractionByIdFallbackMethod(Throwable throwable) {
+        return new AttractionResponseDto(0L, "Temp name", "Temp city", "Temp description");
+    }
 }
